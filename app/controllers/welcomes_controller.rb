9fda39e019c611ed5d18075b36379d0a5a6e5e86
Malson-Lansom <61781906+Malson-Lansom@users.noreply.github.com>
2020-08-17 20:51:50 +0900

    日本語版の調整、文書は修正必須

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,4 +1,12 @@
 class WelcomesController < ApplicationController
+
   def index
   end
+
+  def homepage_eng
+  end
+
+  def homepage_jap
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
   resources :welcomes
-  root 'welcomes#index'
+  get 'homepage_jap', to: 'welcomes#homepage_jap'
+  root 'welcomes#homepage_eng'
 end
